d284fd91b6714c8e320a7a316d155da7bdc1297d
zmpeg <zandstra.matt@gmail.com>
2015-10-01 15:21:54 -0700

    added a controller. Now we can access a read only api

---
+++ CRMService/Controllers/ContactsController.cs
@@ -0,0 +1,34 @@
+﻿using CRMService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRMService.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        Contact[] contacts = new Contact[]
+        {
+            new Contact { Id = 1, FirstName = "Ken", LastName = "Anderson" },
+            new Contact { Id = 2, FirstName = "Trish", LastName = "Anderson" }
+        };
+
+        public IEnumerable<Contact> GetAllContacts()
+        {
+            return contacts;
+        }
+
+        public IHttpActionResult GetContact(int id)
+        {
+            var contact = contacts.FirstOrDefault((p) => p.Id == id);
+            if(contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(contact);
+        }
+    }
+}
