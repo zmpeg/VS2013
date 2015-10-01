074c73ea7d5cf6aa6ac31a0bc97e6d7d797e92b6
zmpeg <zandstra.matt@gmail.com>
2015-10-01 15:13:40 -0700

    added contact model

---
+++ CRMService/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMService.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
